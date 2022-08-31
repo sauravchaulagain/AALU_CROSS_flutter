54847812e03b7ef5b7e9bf9ae4ab3c8cb4202091
Saurav Chaulagain <sauravchaulagain39gmail.com>
2022-08-31 18:25:40 +0545

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,427 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset('assets/aalucross.png'),
+          Container(
+              height: 300, child: Image.asset('assets/TICTACTOELOGO.png')),
+          Text('Developed by:\n Saurav',
+              textAlign: TextAlign.center,
+              style: GoogleFonts.roboto(
+                textStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40,
+                  color: Colors.black,
+                ),
+              )),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => HomePage()));
+            },
+            child: Container(
+              height: 60,
+              width: 180,
+              decoration: BoxDecoration(
+                  color: Colors.deepOrange,
+                  borderRadius: BorderRadius.circular(22)),
+              child: Center(
+                child: Text(
+                  'Play Game!',
+                  style: GoogleFonts.roboto(
+                    textStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool ohTurn = true;
+  int filledBoxes = 0;
+  List<String> displayExOh = [
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: ListView(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: _clearBoard,
+                    icon: Icon(
+                      Icons.restart_alt,
+                      size: 50,
+                      color: Colors.black,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => _information()));
+                    },
+                    icon: Icon(
+                      Icons.info_outline_rounded,
+                      size: 50,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 410,
+                child: GridView.builder(
+                  itemCount: 9,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3),
+                  itemBuilder: (BuildContext context, int index) {
+                    return GestureDetector(
+                      onTap: () {
+                        _tapped(index);
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.red)),
+                        child: Center(
+                          child: Text(
+                            displayExOh[index],
+                            style: GoogleFonts.roboto(
+                              textStyle: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 60,
+                                color: Colors.blueAccent,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Center(
+                child: Text(
+                  'AALU CROSS',
+                  style: GoogleFonts.roboto(
+                    textStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 44,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: Text('Developed by: Saurav',
+                    style: GoogleFonts.roboto(
+                      textStyle: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                        color: Colors.black,
+                      ),
+                    )),
+              ),
+              SizedBox(
+                height: 60,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomeScreen()));
+                },
+                child: Container(
+                    height: 50,
+                    width: 70,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.greenAccent),
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(18)),
+                    child: Icon(
+                      Icons.arrow_back,
+                      size: 50,
+                      color: Colors.white,
+                    )),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _tapped(int index) {
+    setState(() {
+      if (ohTurn && displayExOh[index] == '') {
+        displayExOh[index] = 'O';
+        filledBoxes += 1;
+      } else if (!ohTurn && displayExOh[index] == '') {
+        displayExOh[index] = 'X';
+        filledBoxes += 1;
+      }
+      ohTurn = !ohTurn;
+      _checkWinner();
+    });
+  }
+
+  //displayExOh[]==displayExOh[]
+  void _checkWinner() {
+    if (displayExOh[0] == displayExOh[1] &&
+        displayExOh[0] == displayExOh[2] &&
+        displayExOh[0] != '') {
+      _showWinner(displayExOh[0]);
+    }
+    if (displayExOh[3] == displayExOh[4] &&
+        displayExOh[3] == displayExOh[5] &&
+        displayExOh[3] != '') {
+      _showWinner(displayExOh[3]);
+    }
+    if (displayExOh[6] == displayExOh[7] &&
+        displayExOh[6] == displayExOh[8] &&
+        displayExOh[6] != '') {
+      _showWinner(displayExOh[6]);
+    }
+    if (displayExOh[0] == displayExOh[3] &&
+        displayExOh[0] == displayExOh[6] &&
+        displayExOh[0] != '') {
+      _showWinner(displayExOh[0]);
+    }
+    if (displayExOh[1] == displayExOh[4] &&
+        displayExOh[1] == displayExOh[7] &&
+        displayExOh[1] != '') {
+      _showWinner(displayExOh[1]);
+    }
+    if (displayExOh[2] == displayExOh[5] &&
+        displayExOh[2] == displayExOh[8] &&
+        displayExOh[2] != '') {
+      _showWinner(displayExOh[2]);
+    }
+    if (displayExOh[6] == displayExOh[4] &&
+        displayExOh[6] == displayExOh[2] &&
+        displayExOh[6] != '') {
+      _showWinner(displayExOh[6]);
+    }
+    if (displayExOh[0] == displayExOh[4] &&
+        displayExOh[0] == displayExOh[8] &&
+        displayExOh[0] != '') {
+      _showWinner(displayExOh[0]);
+    } else if (filledBoxes == 9) {
+      _showDraw();
+    }
+  }
+
+  void _showDraw() {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Draw'),
+            actions: [
+              Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.greenAccent),
+                    color: Colors.greenAccent,
+                    borderRadius: BorderRadius.circular(18)),
+                child: GestureDetector(
+                  onTap: () {
+                    _clearBoard();
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Paly Again!'),
+                ),
+              )
+            ],
+          );
+        });
+  }
+
+  void _showWinner(String winner) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Player: ' + winner + ' won'),
+            actions: [
+              Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.greenAccent),
+                    color: Colors.greenAccent,
+                    borderRadius: BorderRadius.circular(18)),
+                child: GestureDetector(
+                  onTap: () {
+                    _clearBoard();
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Paly Again!'),
+                ),
+              )
+            ],
+          );
+        });
+  }
+
+  void _clearBoard() {
+    setState(() {
+      for (int i = 0; i < 9; i++) {
+        displayExOh[i] = '';
+      }
+    });
+    filledBoxes = 0;
+  }
+}
+
+class _information extends StatelessWidget {
+  _information({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(vertical: 150, horizontal: 50),
+        child: Column(
+          children: [
+            Text(
+              'Hello! I am saurav. I developed this game application using flutter. ',
+              style: GoogleFonts.roboto(
+                textStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Colors.black,
+                ),
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              'Get Connected',
+              style: GoogleFonts.roboto(
+                textStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(18),
+                  border: Border.all(color: Colors.black)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      onPressed: () async {
+                        const url =
+                            'https://www.facebook.com/profile.php?id=100082619644241';
+                        if (await canLaunch(url)) {
+                          await launch(url);
+                        } else {
+                          throw 'Could not launch $url';
+                        }
+                      },
+                      icon: Icon(FontAwesomeIcons.facebook)),
+                  IconButton(
+                    onPressed: () async {
+                      const url = 'https://github.com/sauravchaulagain';
+                      if (await canLaunch(url)) {
+                        await launch(url);
+                      } else {
+                        throw 'Could not launch $url';
+                      }
+                    },
+                    icon: Icon(FontAwesomeIcons.github),
+                  ),
+                  IconButton(
+                      onPressed: () async {
+                        const url =
+                            'https://www.linkedin.com/in/saurav-chaulagain-aa368b234/';
+                        if (await canLaunch(url)) {
+                          await launch(url);
+                        } else {
+                          throw 'Could not launch $url';
+                        }
+                      },
+                      icon: Icon(FontAwesomeIcons.linkedin)),
+                  IconButton(
+                      onPressed: () async {
+                        const url = 'https://mail.google.com/mail/u/1/#inbox/';
+                        if (await canLaunch(url)) {
+                          await launch(url);
+                        } else {
+                          throw 'Could not launch $url';
+                        }
+                      },
+                      icon: Icon(Icons.ac_unit_outlined)),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
